17368db98cb157fcb13ed36e3b9338ec90b039ba
Lucrecia Vettori <lucrecia.vettori.riga@gmail.com>
2022-02-10 20:07:18 -0300

    panel administrador

--- AppWeb/admin.py
+++ AppWeb/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from AppWeb.models import *
 
+# Register your models here.
+admin.site.register(Clientes)
+
+admin.site.register(Vendedor)
+
+admin.site.register(Auto)
 
