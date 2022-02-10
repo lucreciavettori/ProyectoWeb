b872b88c76ba4aac529491b0cdca8f457fedb088
Lucrecia Vettori <lucrecia.vettori.riga@gmail.com>
2022-02-10 19:58:26 -0300

    herencias

--- AppWeb/urls.py
+++ AppWeb/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from AppWeb.views import auto, clientes, vendedor
+from AppWeb.views import auto, clientes, inicio, vendedor
 
 urlpatterns = [
     path('clientes/', clientes),
     path('vendedor/', vendedor),
     path('vehiculo/', auto),
+    path('inicio/', inicio),
 ]
--- AppWeb/views.py
+++ AppWeb/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def inicio(request):
+    return render (request, 'AppWeb/inicio.html')
+
 def clientes(request):
-    return render (request, 'AppWeb/index.html')
+    return render (request, 'AppWeb/clientes.html')
 
 def vendedor(request):
     return render (request, 'AppWeb/vendedor.html')
 
 def auto(request):
     return render (request, 'AppWeb/vehiculo.html')
+
